0495e6565fbcdd927240ab044cf09a97e7f59447
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2025-04-30 10:10:29 -0400

    :3:#:3;3:3:3:3:3:3r:3:3:3;3:3
    one day soon I will emerge from this horrific flesh prison and be born anew as a species this world has yet to even BEGIN to comprehend!!

--- gameloop.js
+++ gameloop.js
@@ -1,11 +1,33 @@
-document.addEventListener('load', (e)  => {
-    let player1 = new Player(false, false, "[none]", "[none]", "[none]");
-    let player2 = new Player(false, false, "[none]", "[none]", "[none]");
-    // make player 1 icon show or smth idk
-})
+import {balls} from "./physics.js"
 
-document.addEventListener("STOP", (e) =>{
+let REMAINING_SOLIDS = 0
+let REMAINING_STRIPES =  0
+for (let ball in balls){
+    if (ball.suit == "solid"){
+        REMAINING_SOLIDS++
+    } else if (ball.suit == "stripe") {
+        REMAINING_STRIPES++
+    }
+}
+
+
+
+let player1 = new Player("false", "false", "[none]", "[none]", "[none]")
+let player2 = new Player("false", "false", "[none]", "[none]", "[none]")
+
+document.addEventListener("STOP", playTurn)
+
+let turnNum = 0
+function playTurn(){
+    turnNum++
+    let ballsHappened = //call wills method that returns BALLS
+    if (turnNum%2==0) { // player 2 shish
+        player(player2, ballsHappened)
+    } else { // player 1 shish
+        player(player1, ballsHappened)
+    }
+}
+
+function player(player, balls){
     
-
-    // make the player thing switch
-})
+}
